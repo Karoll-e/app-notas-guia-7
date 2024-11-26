997bc74a519860aba4c9ae60a19bb252761d72cc
Karoll Escalante <karollescalante02@gmail.com>
2024-11-26 16:31:08 -0500

    Add NavBar component to Home page

--- frontend/app-notas/src/pages/Home.jsx
+++ frontend/app-notas/src/pages/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import NavBar from '../components/NavBar'
 
 function Home() {
   return (
+    <>
+    <NavBar/>
     <div className='text-3xl font-bold underline'>Home</div>
+    </>
+    
   )
 }
 
