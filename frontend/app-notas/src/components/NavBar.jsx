2d08e693270f2178265994ab756d520b8e7e8b1e
Karoll Escalante <karollescalante02@gmail.com>
2024-11-26 16:31:03 -0500

    Add NavBar, ProfileInfo, and SearchBar components with search functionality

--- frontend/app-notas/src/components/NavBar.jsx
+++ frontend/app-notas/src/components/NavBar.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState } from "react";
+import ProfileInfo from "./ProfileInfo";
+import { useNavigate } from "react-router-dom";
+import SearchBar from "./SearchBar";
 
 const NavBar = () => {
-  return (
-    <div>NavBar</div>
-  )
-}
+  const [searchQuery, setSearchQuery] = useState("");
+  const navigate = useNavigate;
 
-export default NavBar
+  const onLogout = () => {
+    navigate("/login");
+  };
+
+  const handleSearch = () => {};
+
+  const onClearSearch = () => {
+    setSearchQuery("");
+  };
+
+  return (
+    <div className="bg-white flex items justify-between px-6 py-2 drop-shadow">
+      <img width={30} src="../public/favicon.ico" alt="" className="py-2" />
+      <SearchBar
+        value={searchQuery}
+        onChange={({ target }) => {
+          setSearchQuery(target.value);
+        }}
+        handleSearch={handleSearch}
+        onClearSearch={onClearSearch}
+      />
+      <ProfileInfo onLogout={onLogout} />
+    </div>
+  );
+};
+
+export default NavBar;
