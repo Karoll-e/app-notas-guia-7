b508b3817a560dc0414494e7c8200f4572be8413
Karoll Escalante <karollescalante02@gmail.com>
2024-11-26 17:52:17 -0500

    Add NoteCard component and integrate it into Home page layout

---
+++ frontend/app-notas/src/components/NoteCard.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Pin, Pencil, Trash2 } from "lucide-react";
+
+const NoteCard = ({
+  title,
+  date,
+  content,
+  tags,
+  isPinned,
+  onEdit,
+  onDelete,
+  onPinNote,
+}) => {
+  return (
+    <div className="border rounded p-4 bg-white hover:shadow-xl transition-all ease-in-out">
+      <div className="flex items-center justify-between">
+        <div>
+          <h6 className="text-sm font-medium">{title}</h6>
+          <span className="text-xs text-slate-500">{date}</span>
+        </div>
+        <Pin
+          className={`icon-btn ${isPinned ? "text-primary" : "text-slate-300"}`}
+          onClick={onPinNote}
+        />
+      </div>
+      <p className="text-xs text-slate-600 mt-2">{content?.slice(0, 60)}</p>
+      <div className="flex items-center justify-between mt-2">
+        <div className="text-xs text-slate-500">{tags}</div>
+        <div className="flex items-center gap-3">
+          <Pencil className="icon-btn hover:text-green-600" onClick={onEdit} />
+          <Trash2 className="icon-btn hover:text-red-500" onClick={onDelete} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NoteCard;
--- frontend/app-notas/src/pages/Home.jsx
+++ frontend/app-notas/src/pages/Home.jsx
@@ -1,14 +1,51 @@
-import React from 'react'
-import NavBar from '../components/NavBar'
+import React from "react";
+import NavBar from "../components/NavBar";
+import NoteCard from "../components/NoteCard";
+import { Plus } from 'lucide-react';
 
 function Home() {
   return (
     <>
-    <NavBar/>
-    <div className='text-3xl font-bold underline'>Home</div>
+      <NavBar />
+      <div className="container mx-auto p-4">
+        <div className="grid grid-cols-3 gap-4 mt-8">
+          <NoteCard
+            title="Meeting on Monday"
+            date="Dec 02, 2024"
+            content="lorem"
+            tags="#job"
+            isPinned={true}
+            onEdit={() => {}}
+            onDelete={() => {}}
+            onPinNote={() => {}}
+          />
+          <NoteCard
+            title="Meeting on Monday"
+            date="Dec 02, 2024"
+            content="lorem"
+            tags="#job"
+            isPinned={true}
+            onEdit={() => {}}
+            onDelete={() => {}}
+            onPinNote={() => {}}
+          />
+          <NoteCard
+            title="Meeting on Monday"
+            date="Dec 02, 2024"
+            content="lorem"
+            tags="#job"
+            isPinned={true}
+            onEdit={() => {}}
+            onDelete={() => {}}
+            onPinNote={() => {}}
+          />
+        </div>
+      </div>
+      <button className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10" onClick={()=>{}}>
+        <Plus className="text-[32px] text-white"/>
+      </button>
     </>
-    
-  )
+  );
 }
 
-export default Home
+export default Home;
