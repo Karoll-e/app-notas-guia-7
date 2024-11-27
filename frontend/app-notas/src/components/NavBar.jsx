8613064c8314c07f74572d8b41b86a82c62953db
Karoll Escalante <karollescalante02@gmail.com>
2024-11-26 23:03:14 -0500

    Fix NavBar navigation hook and update favicon path

--- frontend/app-notas/src/components/NavBar.jsx
+++ frontend/app-notas/src/components/NavBar.jsx
@@ -5,7 +5,7 @@ import SearchBar from "./SearchBar";
 
 const NavBar = () => {
   const [searchQuery, setSearchQuery] = useState("");
-  const navigate = useNavigate;
+  const navigate = useNavigate();
 
   const onLogout = () => {
     navigate("/login");
@@ -18,8 +18,8 @@ const NavBar = () => {
   };
 
   return (
-    <div className="bg-white flex items justify-between px-6 py-2 drop-shadow">
-      <img width={30} src="../public/favicon.ico" alt="" className="py-2" />
+    <div className="bg-white flex items justify-between px-24 py-2 drop-shadow">
+      <img width={30} src="/favicon.ico" alt="" className="py-2" />
       <SearchBar
         value={searchQuery}
         onChange={({ target }) => {
